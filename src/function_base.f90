c0de1a90b486663d00c488a866724d3c836aba5f
Tetsuo Koyama <tkoyama010@gmail.com>
2014-07-21 18:11:29 +0900

    add routine
    Function Base

---
+++ src/function_base.f90
@@ -0,0 +1,55 @@
+module function_base
+
+use types, only: dp
+use constants, only: pi, i_
+use utils, only: stop_error
+use optimize, only: bisect
+use amos
+implicit none
+
+private
+public angle
+
+interface angle
+  module procedure angle_real
+  module procedure angle_complex
+end interface angle
+
+contains
+
+function angle_real(z, deg) result(res)
+  implicit none
+  real(dp) :: res
+  real(dp) :: z
+  integer :: deg
+  double precision fact
+  real(dp) :: zimag, zreal
+  if(deg > 0.d0)then
+      fact = 180.d0/pi
+  else
+      fact = 1.d0
+  endif
+  zimag = 0.d0
+  zreal = z  
+  res = atan2(zimag, zreal) * fact
+end function angle_real
+
+function angle_complex(z, deg) result(res)
+  implicit none
+  real(dp) :: res
+  complex(dp*2) :: z
+  integer :: deg
+  double precision fact
+  real(dp) :: zimag, zreal
+  if(deg > 0.d0)then
+      fact = 180.d0/pi
+  else
+      fact = 1.d0
+  endif
+  zimag = aimag(z)
+  zreal = real(z)
+  res = atan2(zimag, zreal) * fact
+end function angle_complex
+
+end module
+
