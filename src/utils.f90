10b5923f4accf4b6050c97e9cfecf14a4e705f36
Tetsuo Koyama <tkoyama010@gmail.com>
2014-07-27 13:22:44 +0900

    add subroutine loadtxt_int
    you can use integer data

--- src/utils.f90
+++ src/utils.f90
@@ -14,7 +14,7 @@ interface str
 end interface
 
 interface loadtxt
-    module procedure loadtxt_real, loadtxt_complex
+    module procedure loadtxt_int, loadtxt_real, loadtxt_complex
 end interface
 
 interface savetxt
@@ -179,6 +179,67 @@ subroutine stop_error(msg)
 character(len=*) :: msg ! Message to print on stdout
 print *, msg
 stop 1
+end subroutine
+
+subroutine loadtxt_int(filename, d)
+! Loads a 2D array from a text file.
+!
+! Arguments
+! ---------
+!
+! Filename to load the array from
+character(len=*), intent(in) :: filename
+! The array 'd' will be automatically allocated with the correct dimensions
+integer, allocatable, intent(out) :: d(:, :)
+!
+! Example
+! -------
+!
+! integer, allocatable :: data(:, :)
+! call loadtxt("log.txt", data)  ! 'data' will be automatically allocated
+!
+! Where 'log.txt' contains for example::
+!
+!     1 2 3
+!     2 4 6
+!     8 9 10
+!     11 12 13
+!     ...
+!
+character :: c
+integer :: s, ncol, nrow, ios, i
+logical :: lastwhite
+integer :: r
+
+open(newunit=s, file=filename, status="old")
+
+! determine number of columns
+ncol = 0
+lastwhite = .true.
+do
+   read(s, '(a)', advance='no', iostat=ios) c
+   if (ios /= 0) exit
+   if (lastwhite .and. .not. whitechar(c)) ncol = ncol + 1
+   lastwhite = whitechar(c)
+end do
+
+rewind(s)
+
+! determine number or rows
+nrow = 0
+do
+   read(s, *, iostat=ios) r
+   if (ios /= 0) exit
+   nrow = nrow + 1
+end do
+
+rewind(s)
+
+allocate(d(nrow, ncol))
+do i = 1, nrow
+    read(s, *) d(i, :)
+end do
+close(s)
 end subroutine
 
 subroutine loadtxt_real(filename, d)
